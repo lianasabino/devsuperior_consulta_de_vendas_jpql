13f58a3d6802ddc2728ee0d769d53e7aa775d0ea
lianasabino <lianasabino@hotmail.com>
2025-02-10 09:49:42 -0300

    service and controller summary

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,13 +1,20 @@
 package com.devsuperior.dsmeta.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
@@ -29,4 +36,14 @@ public class SaleController {
 		return null;
 	}
 
+	@GetMapping(value = "/summary")
+	public ResponseEntity<Page<SaleSummaryDTO>> getSummary(
+			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate minDate,
+			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate maxDate,
+			Pageable pageable
+			)
+	{
+		Page<SaleSummaryDTO> dto = service.findSummary(minDate, maxDate, pageable);
+		return ResponseEntity.ok(dto);
+	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -21,6 +25,26 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+	
+
+	@Transactional(readOnly = true)
+	public Page<SaleSummaryDTO> findSummary(LocalDate minDate, LocalDate maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.now();
+		
+		LocalDate initialDate;
+	
+		if(minDate != null) {
+			initialDate = minDate;
+		} else if(minDate == null && maxDate != null) {
+			initialDate = maxDate.minusYears(1);
+		} else {
+			initialDate = today.minusYears(1);
+		}
+				
+        LocalDate finalDate = maxDate != null ? maxDate : today;  
+        
+        return repository.searchSummaryByDate(initialDate, finalDate, pageable);
 	}
 	
 	
